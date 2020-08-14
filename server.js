631884ebe5f8b40f59da9ed90f3526ddf81996ea
Mad Hallé <mad.halle@icloud.com>
2020-08-13 19:55:16 -0600

    graphiql web interface guery variables not responding

---
+++ server.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql').graphqlHTTP;
+var { buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    user(id: Int!): Person
+    users(shark: String): [Person]
+  },
+  type Person {
+    id: Int!
+    name: String
+    age: Int!
+    shark: String
+  }
+`);
+
+var users = [
+  {
+    id: 1,
+    name: 'Brian',
+    age: 21,
+    shark: 'Great White Shark'
+  },
+  {
+    id: 2,
+    name: 'Kim',
+    age: 22,
+    shark: 'Whale Shark'
+  },
+  {
+    id: 3,
+    name: 'Faith',
+    age: 23,
+    shark: 'Hammerhead Shark'
+  },{
+    id: 4,
+    name: 'Joseph',
+    age: 23,
+    shark: 'Tiger Shark'
+  },
+  {
+    id: 5,
+    name: 'Joy',
+    age: 25,
+    shark: 'Hammerhead Shark'
+  },
+];
+
+var getUser = function(args){
+  var UserID = args.id;
+  return users.filter(user => user.id == userID)[0];
+}
+
+var retrieveUsers = function(args){
+  if (args.shark) {
+    var shark = args.shark;
+    return users.filter(user => user.shark === shark);
+  } else {
+    return users;
+  }
+}
+
+var root = {
+  user: getUser,
+  users: retrieveUsers
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+})
+);
+
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
